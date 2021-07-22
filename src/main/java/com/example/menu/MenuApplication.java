0b24a2a7944ba9b34796042fc90a7c55f9d8ee7b
Phạm Văn Điệp (VIN3S-KCN-PNTAPI) <dieppv1@vingroup.net>
2021-07-22 14:43:04 +0700

    Initial commit

---
+++ src/main/java/com/example/menu/MenuApplication.java
@@ -0,0 +1,18 @@
+package com.example.menu;
+/**
+ * Domain Driven
+ * com
+ *  +- example
+ *    +- menu
+ *      +- MenuApplication.java
+ *      |
+ *      +- item
+ *      |  +- Item.java
+ *      |  +- ItemController.java
+ *      |  +- ItemService.java
+ *      |  +- InMemoryItemRepository.java
+ *
+ * **/
+public class MenuApplication {
+
+}
