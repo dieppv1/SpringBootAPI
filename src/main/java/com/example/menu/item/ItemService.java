9f3b6b6fec7d3534bcdb5bac95183d03c10b04ad
dieppv1 <dieppv0306@gmail.com>
2021-07-23 14:36:47 +0700

    add more jar file

--- src/main/java/com/example/menu/item/ItemService.java
+++ src/main/java/com/example/menu/item/ItemService.java
@@ -24,7 +24,8 @@ public class ItemService {
         return List.of(
                 new Item(1L, "Burger", 599L, "Tasty", "https://cdn.auth0.com/blog/whatabyte/burger-sm.png"),
                 new Item(2L, "Pizza", 299L, "Cheesy", "https://cdn.auth0.com/blog/whatabyte/pizza-sm.png"),
-                new Item(3L, "Tea", 199L, "Informative", "https://cdn.auth0.com/blog/whatabyte/tea-sm.png")
+                new Item(3L, "Tea", 199L, "Informative", "https://cdn.auth0.com/blog/whatabyte/tea-sm.png"),
+                new Item(4L, "diep", 199L, "diep hello", "https://cdn.auth0.com/blog/whatabyte/tea-sm.png")
         );
     }
 
